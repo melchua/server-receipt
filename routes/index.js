0e62ecb53cb78d74fcfbc13045aef915ad9e8c1b
billywoo17 <billwu_@outlook.com>
2018-05-31 16:26:22 -0700

    fixed changes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.send("hello");
-=======
   res.render('index', { title: 'Receipt' });
->>>>>>> f6ed3bcd6efbf89a488f6d98d337028fc185e126
 });
 
 module.exports = router;
