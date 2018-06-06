960d8c50c4df1b417ec1db45b08213e581baa26a
Mel Chua <mchuam@gmail.com>
2018-06-05 18:53:14 -0700

    added projects routing

---
+++ routes/projects.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+
+const database = require("./data/database");
+const bodyParser = require('body-parser');
+
+
+/* GET /projects */
+/* returns project listing of all projects */
+router.get('/', function (req, res, next) {
+  // console.log("route is working!!!!");
+   database.returnProjectList()
+     .then((result) => {
+       console.log('rows: ', result);
+       res.send(result.rows);
+     }).catch((err) => console.error("Error:", err));
+});
+
+module.exports = router;
