434ed8a40fc33f85b69548fb57f17a12b1586603
Mel Chua <mchuam@gmail.com>
2018-06-06 13:06:45 -0700

    prior TI

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,7 @@ router.post('/login', function (req, res, next) {
       if (result[0]) {
         const token = createToken(email, password, result[0].admin);
         console.log("Created new token: ", token);
-        res.json({token}); // response.token
+        res.json({token, admin: result[0].admin}); // response.token
       } else {
         console.log("no way buddy");
         res.send(401);
@@ -53,29 +53,29 @@ router.get('/receipts', function (req, res, next) {
          console.log("email: ", token.email);
          console.log("password", token.password);
          const userId = result[0].id;
-         database.returningReceipts(userId)
+         database.returnAllReceipts()
            .then((result) => {
              console.log('rows: ', result);
              res.send(result.rows);
            });
        });
-   }
+    }
   });
 });
 
 // Testing for the above
 
-// router.get('/:id/receipts', function (req, res, next) {
+// router.get('/testme', function (req, res, next) {
 //      console.log("in the thing right now");
-//      database.validateLogin("", "")
-//        .then((result) => {
-//          const userId = result[0].id;
-//          console.log("user id:", userId);
-//          database.returningReceipts(userId)
+//      // database.validateLogin("", "")
+//      //   .then((result) => {
+//      //     const userId = result[0].id;
+//      //     console.log("user id:", userId);
+//          database.returnAllReceipts()
 //            .then((result) => {
 //              res.send(result.rows);
 //            });
-//        });
+//        // });
 // });
 
 
